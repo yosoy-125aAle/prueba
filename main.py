a0c4a79be5ea5f077b1410bb7829a6e70fdb3068
Jesús Alejandro Jiménez Castellanos <jesus.jimenez7858@alumnos.udg.mx>
2025-11-01 09:25:07 -0600

    Recortar caracteres de texto

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ usar_linter = input("¿Quieres análisis de código estático? (S/n): ") or "s"
 steps_linter = ''
 linter_pkg = ''
 if usar_linter.lower() == 's':
-    linter = input("¿Qué herramienta usarás para código estático? ('flake8', 'pylint', etc. por defecto: 'flake8'): ") or "flake8"
+    linter = input("¿Qué herramienta usarás para código estático? ") or "flake8"
     linter_pkg = linter.split()[0]
-    cmd_linter = input(f"Comando para análisis estático (por defecto: '{linter} .'): ") or f"{linter} ."
+    cmd_linter = input(f"Comando '{linter} .'): ") or f"{linter} ."
     steps_linter = f"""
     - name: Instalar herramienta de análisis estático
       run: pip install {linter_pkg}
@@ -34,9 +34,9 @@ usar_security = input("¿Quieres análisis de seguridad? (S/n): ") or "s"
 steps_security = ''
 security_pkg = ''
 if usar_security.lower() == 's':
-    security = input("¿Herramienta para análisis de seguridad ('bandit', 'safety', etc. por defecto: 'bandit'): ") or "bandit"
+    security = input("¿Herramienta para análisis de seguridad  ") or "bandit"
     security_pkg = security.split()[0]
-    cmd_security = input(f"Comando para análisis de seguridad (por defecto: '{security} .'): ") or f"{security} ."
+    cmd_security = input(f"Comando  '{security} .'): ") or f"{security} ."
     steps_security = f"""
     - name: Instalar herramienta de análisis de seguridad
       run: pip install {security_pkg}
